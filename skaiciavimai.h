4dfe9a1a41efdf367acf020f2744b2cfee4fc856
Skaferis <95220690+Skaferis@users.noreply.github.com>
2026-03-27 11:43:50 +0200

    5 commitas
    Skaiciavimas.h pakeistas is deque i vector skaiciams

--- skaiciavimai.h
+++ skaiciavimai.h
@@ -2,9 +2,8 @@
 #define SKAICIAVIMAI_H
 
 #include <vector>
-#include <deque>
 
-double skaiciuotiMediana(std::deque<int> paz);
+double skaiciuotiMediana(std::vector<int> paz);
 double skaiciuotiGalutini(int suma, int kiekis, int egzaminas);
 
 #endif // SKAICIAVIMAI_H
