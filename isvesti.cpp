9129a5b0ed5a1c6dd68c57dae3e3fd5d179252ab
Skaferis <95220690+Skaferis@users.noreply.github.com>
2026-04-03 12:42:46 +0300

    3 commitas, sutvarkytas isvesti.cpp

--- isvesti.cpp
+++ isvesti.cpp
@@ -8,7 +8,6 @@ using std::cout;
 using std::left;
 
 void spausdinti(const std::vector<Studentas>& grupe, bool rodytiMediana) {
-
     cout << left
          << std::setw(12) << "Vardas"
          << std::setw(15) << "Pavarde";
@@ -21,16 +20,15 @@ void spausdinti(const std::vector<Studentas>& grupe, bool rodytiMediana) {
     cout << "\n";
 
     for (const Studentas& s : grupe) {
-
         cout << left
-             << std::setw(12) << s.vardas
-             << std::setw(15) << s.pavarde
+             << std::setw(12) << s.vardas()
+             << std::setw(15) << s.pavarde()
              << std::fixed << std::setprecision(2);
 
         if (rodytiMediana)
-            cout << std::setw(8) << s.med;
+            cout << std::setw(8) << s.med();
         else
-            cout << std::setw(8) << s.rez;
+            cout << std::setw(8) << s.rez();
 
         cout << "\n";
     }
